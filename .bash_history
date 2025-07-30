1a6221d4da451e0031817510ea7e79a2d8c90441
piyush <piyushkumar16@protonmail.com>
2025-07-30 15:27:19 +0530

    Rewrote code, added choices for setting fish or neovim. added package checks for fish neovim user and docker setups

---
+++ .bash_history
@@ -0,0 +1,105 @@
+pwd
+cd root
+pwd
+ls
+ls -la
+chmow 700 setup-linux.sh 
+chmod 700 setup-linux.sh 
+./setup-linux.sh 
+clear
+exit
+dkpg -S curl
+echo $?
+dkpg -S ls
+sudo apt install dpkg
+apt install dpkg
+dpkg -S curl
+clear
+dpkg -S 
+echo $?
+dpkg -S cat
+echo $?
+clear
+ls
+exit
+cd root
+ls -la
+./setup-linux.sh 
+exit
+./setup-linux.sh 
+/root
+cd root
+./setup-linux.sh 
+bash << 'EOF'
+    if dpkg -S curl &> /dev/null; then
+        echo -e "\033[36m Curl found.....\036[0m"
+    else
+        echo -e "\033[33mCurl not found, installing.....\033[0m"
+        apt install -y curl
+    fi
+EOF
+
+curl
+apt install curl
+bash << 'EOF'
+    if dpkg -S curl &> /dev/null; then
+        echo -e "\033[36m Curl found.....\036[0m"
+    else
+        echo -e "\033[33mCurl not found, installing.....\033[0m"
+        apt install -y curl
+    fi
+EOF
+
+exit
+./setup-linux.sh 
+cd root
+./setup-linux.sh 
+./setup-linux.sh 
+exit
+./setup-linux.sh 
+cd /root
+./setup-linux.sh 
+./setup-linux.sh 
+sudo apt install 
+sudo apt install htop
+curl https://get.docker.com | sh
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_12/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/fish-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/fish-archive-keyring.gpg] http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_12/ /" | sudo tee /etc/apt/sources.list.d/fish.list
+sudo apt update
+exit
+cd /root
+./setup-linux.sh
+cat /etc/os-release
+./setup-linux.sh
+exit
+exit
+cd root
+./setup-linux.sh 
+/root/setup-linux.sh 
+/root/setup-linux.sh 
+/root/setup-linux.sh 
+/root/setup-linux.sh 
+exit
+/root/setup-linux.sh 
+/root/setup-linux.sh 
+/root/setup-linux.sh 
+/root/setup-linux.sh 
+-d /root/.ssh
+bash << 'EOF'
+    if [[ -d /root/.ssh ]]; then 
+        echo "/root/ssh"
+    fi
+EOF
+
+ls /root/.ssh
+exit
+exit
+apt install sudo
+sudo
+apt install sudo2
+apt search sudo
+exit
+apt install sudo
+apt update
+apt install sudo
+exit
--- setup-linux.sh
+++ setup-linux.sh
@@ -2,54 +2,130 @@
 
 install_neovim_latest() {
     echo -e "\033[36mInstalling neovim.....\033[0m"
-    sleep 2
-    curl -sL https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz | sudo tar -xzf - --strip-components=1 --overwrite -C /usr
+    . /etc/os-release
+
+    case $ID in
+        debian|ubuntu)
+        REQUIRED_PKG="curl"
+        PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+        echo Checking for $REQUIRED_PKG: $PKG_OK
+        if [ "" = "$PKG_OK" ]; then
+            echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+            apt install $REQUIRED_PKG -y
+        fi
+        curl -f https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz | sudo tar -xzvf - --strip-components=1 --overwrite -C /usr
+        ;;
+
+        fedora)
+            dnf install neovim -y
+        ;;
+
+        arch)
+            pacman -S --noconfirm --needed neovim
+        ;;
+
+        *) echo "This is an unknown distribution."
+        ;;
+    esac
+            
 }
 
-setup_debian() {
-    if [ "$VERSION_ID" -eq "13" ]; then VERSION_ID="Unstable"; fi
-    apt update
-    apt install sudo gpg -y
-    echo -e "\033[36mInstalling fish shell.....\033[0m"
-    sleep 2
-    echo "deb http://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_$VERSION_ID/ /" | sudo tee /etc/apt/sources.list.d/shells:fish:release:4.list
-    curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:4/Debian_12/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_4.gpg > /dev/null
-    sudo apt update
-    sudo apt install fish -y
-    install_neovim_latest
-    echo "\033[31mDebian initiation completed.....\033[0m"
-}
+install_fish_latest() {
+    . /etc/os-release
 
-setup_ubuntu() {
-    apt update
-    apt install sudo -y
-    echo -e "\033[36mInstalling fish shell.....\033[0m"
-    sleep 2
-    sudo add-apt-repository ppa:fish-shell/release-4
-    sudo apt update
-    sudo apt install fish -y
-    install_neovim_latest
-    echo -e "\033[31mUbuntu initiation completed.....\033[0m"
+    case $ID in
+        debian)
+            echo -e "\033[36mDebian \033[0m"
+            REQUIRED_PKG="wget"
+            PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+            echo Checking for $REQUIRED_PKG: $PKG_OK
+            if [ "" = "$PKG_OK" ]; then
+                echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+                apt update
+                apt install $REQUIRED_PKG -y
+            fi
+            if [ "$VERSION_ID" -eq "13" ]; then VERSION_ID="Unstable"; fi
+            wget -qO fish.deb "https://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_$VERSION_ID/amd64/fish_4.0.2-2_amd64.deb"
+            apt install $(pwd)/fish.deb -y
+            rm -f fish.deb
+        ;;
+
+        ubuntu)
+            echo -e "\033[36mUbuntu \033[0m"
+            apt update
+            echo -e "\033[36mInstalling fish shell.....\033[0m"
+            add-apt-repository ppa:fish-shell/release-4
+            apt update
+            apt install fish -y
+        ;;
+        fedora)
+            echo -e "\033[36mFedora \033[0m"
+            dnf install fish -y
+        ;;
+
+        arch)
+            echo -e "\033[36mArch 󰣇\033[0m"
+            pacman -S --noconfirm --needed fish
+        ;;
+
+        *) echo " This is an unknown distribution ."
+        ;;
+    esac
+
 }
 
 setup_docker() {
     curl -fsSL https://get.docker.com | sh
-    echo -e "\033[32mDocker Installed.....\033[0m"
-    if [[ "$USER_FLAG" -eq 1 ]]; then usermod -aG docker $USERNAME; fi
-    echo -e "\033[31mDocker Installed .....\033[0m"
+    if [[ -n $USERNAME ]]; then 
+        usermod -aG docker $USERNAME
+        echo -e "\033[36m$USERNAME added to docker group\033[0m"
+    fi
+    echo -e "\033[36mDocker Installed .....\033[0m"
 }
 
 setup_user() {
+
+    echo "Setting User..."
+    
+  
+    . /etc/os-release
+
+    case $ID in
+        debian|ubuntu)
+            echo -e "\033[36mDebian |Ubuntu \033[0m"
+            REQUIRED_PKG="sudo"
+            PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+            echo Checking for $REQUIRED_PKG: $PKG_OK
+            if [ "" = "$PKG_OK" ]; then
+                echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."  
+                apt update
+                apt install $REQUIRED_PKG -y
+            fi
+        ;;
+        fedora)
+            echo -e "\033[36mFedora \033[0m"
+            dnf install sudo -y
+        ;;
+
+        arch)
+            echo -e "\033[36mArch 󰣇\033[0m"
+            pacman -S --noconfirm --needed sudo
+        ;;
+
+        *) echo " This is an unknown distribution ."
+        ;;
+    esac
+
     echo "Enter Username"
     read USERNAME
     if [[ -z "$USERNAME" ]]; then echo "\033[31mUSERNAME not set.....\033[0m"; return; fi
     sugroup=$(getent group | grep -E "(wheel|sudo)" | head -n1 | cut -d: -f1)
     case $sugroup in
         sudo)
-            useradd -m -u 1000 -s /usr/bin/fish $USERNAME -G sudo
+            useradd -m -s /usr/bin/fish $USERNAME -G sudo
             ;;
         wheel)
-            useradd -m -u 1000 -s /usr/bin/fish $USERNAME -G wheel
+            useradd -m -s /usr/bin/fish $USERNAME -G wheel
             ;;
         *)
             echo "sudo is not ready"
@@ -59,60 +135,72 @@ setup_user() {
         cp -r /root/.ssh /home/$USERNAME/.ssh
         chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
     fi
-    echo -e "\033[33mUser Created, excecuted passwd $USERNAME to setup password.....\033[0m"
-    USER_FLAG=1
+    echo -e "\033[36mUser Created. Ssh keys copied. Excecute passwd $USERNAME to setup password\033[0m"
 }
 
-setup_dnf_distros() {
-        dnf update
-        dnf install --skip-broken sudo gpg unzip -y
-        sudo dnf install fish neovim -y
-}
+setup_wrapper() {
 
-. /etc/os-release
-
-case $ID in
-    debian)
-        setup_debian
-    ;;
-
-    ubuntu)
-        setup_ubuntu
-    ;;
-
-    fedora)
-        setup_dnf_distros
-    ;;
-
-    arch)
-        pacman -Sy --needed --noconfirm sudo fish neovim
-    ;;
-
-    *) echo "This is an unknown distribution."
-    ;;
-    
+    echo -e "\033[33mInstall fish (y/n)?\033[0m"
+    read choice
+    case $choice in 
+        y|Y|yes|YES)
+            install_fish_latest
+            echo -e "\033[32mFish installed.....\033[0m"
+            ;;
+        n|N|no|NO) echo "Skipped fish..."
+            ;;
+        *) echo "Invalid input"
+            ;;
     esac
 
-echo -e "\033[33mSetup User? (y/n).....\033[0m" 
-read choice
-case $choice in
-    y|Y|yes|YES)
-        setup_user
-    ;;
-    n|N|no|NO) echo "Skipped..."
-    ;;
-    *) echo "Invalid input"
-    ;;
-esac
+   echo -e "\033[33mInstall neovim (y/n)?\033[0m"
+    read choice
+    case $choice in 
+        y|Y|yes|YES)
+            install_neovim_latest
+            echo -e "\033[32mNeovim installed.....\033[0m"
+            ;;
+        n|N|no|NO) echo "Skipped neovim..."
+            ;;
+        *) echo "Invalid input"
+            ;;
+    esac
 
-echo -e "\033[33mSetup Docker? (y/n).....\033[0m" 
-read choice
-case $choice in
-    y|Y|yes|YES)
-        setup_docker
-    ;;
-    n|N|no|NO) echo "Skipped..."
-    ;;
-    *) echo "Invalid input"
-    ;;
-esac
+    echo -e "\033[33mSetup User? (y/n).....\033[0m" 
+    read choice
+    case $choice in
+        y|Y|yes|YES)
+            setup_user
+        ;;
+        n|N|no|NO) echo "Skipped..."
+        ;;
+        *) echo "Invalid input"
+        ;;
+    esac
+
+    echo -e "\033[33mSetup Docker? (y/n).....\033[0m" 
+    read choice
+    case $choice in
+        y|Y|yes|YES)
+            setup_docker
+        ;;
+        n|N|no|NO) echo "Skipped..."
+        ;;
+        *) echo "Invalid input"
+        ;;
+    esac
+}
+
+main() {
+
+    echo "Check root..."
+    if [[ $UID -ne 0 ]]; then
+        echo "Error: This script must be run as root" >&2
+        exit 1
+    fi 
+    echo "Running as root "
+    setup_wrapper
+    echo -e "\033[32mCompleted\033[0m"
+}
+
+main "$@"
