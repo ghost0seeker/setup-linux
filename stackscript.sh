5f624e6909b26e1f418d3eefa214f000b95ce33a
Piyush <piyushkumar16@protonmail.com>
2026-07-03 20:27:11 +0530

    Working on stackscript

--- stackscript.sh
+++ stackscript.sh
@@ -1,126 +1,17 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+echo "tzdata tzdata/Areas select Asia" | debconf-set-selections
+echo "tzdata tzdata/Zones/Asia select Kolkata" | debconf-set-selections
 
-# Installing Fish
+apt update; apt upgrade -y
 
-. /etc/os-release
+apt install -y python3 python3-pip
 
-case $ID in
-    debian)
-        echo -e "\033[36mDebian \033[0m"
-        REQUIRED_PKG="wget"
-        PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-        echo Checking for $REQUIRED_PKG: $PKG_OK
-        if [ "" = "$PKG_OK" ]; then
-            echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-            apt update
-            apt install $REQUIRED_PKG -y
-        fi
-        wget -qO fish.deb "https://download.opensuse.org/repositories/shells:/fish:/release:/4/Debian_$VERSION_ID/amd64/fish_4.0.2-2_amd64.deb"
-        apt install $(pwd)/fish.deb -y
-        rm -f fish.deb
-    ;;
+cd /root/setup-linux
 
-    ubuntu)
-        echo -e "\033[36mUbuntu \033[0m"
-        apt update
-        echo -e "\033[36mInstalling fish shell.....\033[0m"
-        add-apt-repository ppa:fish-shell/release-4
-        apt update
-        apt install fish -y
-    ;;
-    fedora)
-        echo -e "\033[36mFedora \033[0m"
-        dnf install fish -y
-    ;;
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirments.txt
+python3 setup.py
 
-    arch)
-        echo -e "\033[36mArch 󰣇\033[0m"
-        pacman -S --noconfirm --needed fish
-    ;;
 
-    *) echo " This is an unknown distribution ."
-    ;;
-esac
-
-# Installing neovim
-echo -e "\033[36mInstalling neovim.....\033[0m"
-
-case $ID in
-    debian|ubuntu)
-    REQUIRED_PKG="curl"
-    PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-    echo Checking for $REQUIRED_PKG: $PKG_OK
-    if [ "" = "$PKG_OK" ]; then
-        echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-        apt install $REQUIRED_PKG -y
-    fi
-    curl -f https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz | sudo tar -xzvf - --strip-components=1 --overwrite -C /usr
-    ;;
-
-    fedora)
-        dnf install neovim -y
-    ;;
-
-    arch)
-        pacman -S --noconfirm --needed neovim
-    ;;
-
-    *) echo "This is an unknown distribution."
-    ;;
-esac
-
-# setup user account
-echo "Setting User..."
-
-case $ID in
-    debian|ubuntu)
-        echo -e "\033[36mDebian |Ubuntu \033[0m"
-        REQUIRED_PKG="sudo"
-        PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-        echo Checking for $REQUIRED_PKG: $PKG_OK
-        if [ "" = "$PKG_OK" ]; then
-            echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."  
-            apt update
-            apt install $REQUIRED_PKG -y
-        fi
-    ;;
-    fedora)
-        echo -e "\033[36mFedora \033[0m"
-        dnf install sudo -y
-    ;;
-
-    arch)
-        echo -e "\033[36mArch 󰣇\033[0m"
-        pacman -S --noconfirm --needed sudo
-    ;;
-
-    *) echo " This is an unknown distribution ."
-    ;;
-esac
-
-export USERNAME=ghost
-if [[ -z "$USERNAME" ]]; then echo "\033[31mUSERNAME not set.....\033[0m"; return; fi
-sugroup=$(getent group | grep -E "(wheel|sudo)" | head -n1 | cut -d: -f1)
-case $sugroup in
-    sudo)
-        useradd -m -s /usr/bin/fish $USERNAME -G sudo
-        ;;
-    wheel)
-        useradd -m -s /usr/bin/fish $USERNAME -G wheel
-        ;;
-    *)
-        echo "sudo is not ready"
-        ;;
-esac
-if [[ -d /root/.ssh ]]; then 
-    cp -r /root/.ssh /home/$USERNAME/.ssh
-    chown -R $USERNAME:$USERNAME /home/$USERNAME/.ssh
-fi
-
-# Install docker
-
-curl -fsSL https://get.docker.com | sh
-if [[ -n $USERNAME ]]; then 
-    usermod -aG docker $USERNAME
-    echo -e "\033[36m$USERNAME added to docker group\033[0m"
-fi
-echo -e "\033[36mDocker Installed .....\033[0m"
--- tests.py
+++ tests.py
@@ -1,12 +1,32 @@
+from re import S
 from unittest import TestCase
 import docker
+from docker.types import Mount
 from distros.ubuntu import Ubuntu
 from distros.base_distro import BaseDistro
+from pathlib import Path
 
-class TestStepLinux(TestCase):
+
+class TestSetupLinux(TestCase):
     
     def setUp(self):
+        cwd = Path.cwd()
         self.ubuntu = Ubuntu()
+        self.client = docker.from_env()
+        self.mounts = [
+            Mount(
+                target="/root/setup.py",
+                source=f'{cwd.resolve()}/setup.py',
+                type='bind',
+                read_only=True,
+            ),
+            Mount(
+                target='/root/stackscript.sh',
+                source=f'{cwd.resolve()}/stackscript.sh',
+                type='bind',
+                read_only=True
+            )
+        ]
         
     def test_download_file(self):
         url = "https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz"
@@ -26,4 +46,12 @@ class TestStepLinux(TestCase):
     def test_install_kitty(self):
         apps = ["kitty"]
         self.ubuntu.install_kitty()
+    
+    def test_ubuntu_docker(self):
+        output = self.client.containers.run(
+            "ubuntu:24.04",
+            "/root/stackscript.sh",
+            mounts=self.mounts
+        )
+        print(output.decode('utf-8'))
     
